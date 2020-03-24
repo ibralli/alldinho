e4eef42cbb6a2277e3762946bb2265aa91106549
ibralli <62517743+ibralli@users.noreply.github.com>
2020-03-24 10:28:43 +0000

    Add files via upload

---
+++ Loop Way.py
@@ -0,0 +1,11 @@
+i = int(input("Please enter number "))   #variable 
+for i in range (1, 102):      # loop code
+     sqaure = i*i               # to create squared value 
+     print(i, sqaure)      # Print loop and print squared number
+     if i*i >=200:       #if greater than condition 200 if statement
+         break            #if condition is met then stop loop
+
+     #print(i, n*n)  
+    
+    # if i == 14:
+       # break     early assumption
